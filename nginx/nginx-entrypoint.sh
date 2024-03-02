a71df05d3086252740f2a69259c5010468e54733
Jenish Paul <jenis1303@gmail.com>
2024-03-02 21:16:28 +0530

    Fix

--- nginx/nginx-entrypoint.sh
+++ nginx/nginx-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # add crontab entry to renew the letsencrypt certificate
 # this cron job will run every day 11.00 P.M
-echo "adding crontab"
-echo "0 23 * * * certbot renew --dry-run" | crontab -
+# echo "adding crontab"
+# echo "0 23 * * * certbot renew --dry-run" | crontab -
 
 nginx
