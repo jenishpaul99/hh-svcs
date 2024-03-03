32b6948a617dd09d209d5336985ab8aa8f83064c
Jenish Paul <jenis1303@gmail.com>
2024-03-03 23:42:35 +0530

    Add renew script

---
+++ nginx/renew-certificate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt update -y && apt upgrade -y
+apt install software-properties-common -y
+add-apt-repository 'deb http://ftp.us.debian.org/debian/pool/main/p/python-certbot/certbot_2.1.0-4_all.deb bookworm main' -y
+apt install certbot -y && apt install python3-certbot-nginx -y
+mkdir -p  /var/www/html
+certbot certonly --nginx --force-renewal -d helping-hands.duckdns.org
